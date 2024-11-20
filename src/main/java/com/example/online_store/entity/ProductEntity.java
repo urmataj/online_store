a04e7defa77db9d6d67e2cdc0718f7dc99bcc2e4
Урматай <toktosunovurmataj@gmail.com>
2024-11-20 07:46:18 +0600

    /

---
+++ src/main/java/com/example/online_store/entity/ProductEntity.java
@@ -0,0 +1,25 @@
+package com.example.online_store.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Integer stockQuantity; // Количество на складе
+}
